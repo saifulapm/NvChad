0e9df8ade81fcc49ae7a2378339897d47d456b39
Saiful Islam <saiful.apm@gmail.com>
2024-05-31 14:26:36 +0600

    chore (MasonInstallAll): Will install only which not installed
    Hi.. Your MasonInstallAll command was installing all package everytime when run MasonInstallAll command. 
    
    But we can make it like that so It will install only missing package not all. 
    
    I think this is better approach. 
    
    Anyway, Thanks for great work.

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -55,11 +55,13 @@ return {
       require("mason").setup(opts)
 
       -- custom nvchad cmd to install all mason binaries listed
-      vim.api.nvim_create_user_command("MasonInstallAll", function()
-        if opts.ensure_installed and #opts.ensure_installed > 0 then
-          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+      vim.api.nvim_create_user_command('MasonInstallAll', function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = require('mason-registry').get_package(tool)
+          if not p:is_installed() then p:install() end
         end
       end, {})
+
 
       vim.g.mason_binaries_list = opts.ensure_installed
     end,
